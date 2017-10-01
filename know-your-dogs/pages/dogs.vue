b0ff6b7f5f302ac17ea2e1634966c7588016041f
zuojj <cuew1987@163.com>
2017-10-01 17:23:29 +0800

    update

---
+++ know-your-dogs/pages/dogs.vue
@@ -0,0 +1,42 @@
+<template>
+  <section class="container">
+    <div>
+      <img :src="breed" alt="">
+      <h4>{{name && name.toUpperCase()}}</h4>
+    </div>
+  </section>
+</template>
+
+<script>
+import axios from 'axios'
+export default {
+  async asyncData ({params}) {
+    const { data } = await axios.get(`https://dog.ceo/api/breed/${params.breed}/images/random`)
+    return { breed: data.message, name: params.breed }
+  }
+}
+</script>
+
+<style>
+.container
+{
+  min-height: 100vh;
+  display: flex;
+  justify-content: center;
+  /* align-items: center; */
+  text-align: center;
+}
+img
+{
+  display: inline-block;
+  border-radius: 4px;
+  border: 4px solid #3b8070;
+  color: #3b8070;
+  text-decoration: none;
+  margin-top: 10px;
+}
+h4 {
+  margin-top: 10px;
+  color: #3b8070;
+}
+</style>
---
+++ know-your-dogs/pages/index.vue
@@ -0,0 +1,55 @@
+<template>
+  <section class="container">
+    <div>
+      <a v-for="breed in breeds" :key="breed" class="breed">
+        {{breed}}
+      </a>
+    </div>
+  </section>
+</template>
+
+<script>
+import Logo from '~/components/Logo.vue'
+import axios from 'axios'
+export default {
+
+  async asyncData () {
+    const { data } = await axios.get('https://dog.ceo/api/breeds/list')
+    return { breeds: data.message }
+  },
+  components: {
+    Logo
+  }
+}
+</script>
+
+<style>
+.container {
+  min-height: 100vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  text-align: center;
+}
+
+.title {
+  font-family: "Quicksand", "Source Sans Pro", -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, "Helvetica Neue", Arial, sans-serif; /* 1 */
+  display: block;
+  font-weight: 300;
+  font-size: 100px;
+  color: #35495e;
+  letter-spacing: 1px;
+}
+
+.subtitle {
+  font-weight: 300;
+  font-size: 42px;
+  color: #526488;
+  word-spacing: 5px;
+  padding-bottom: 15px;
+}
+
+.links {
+  padding-top: 15px;
+}
+</style>
