6b0e4fb999e010fb1c8dcbace31c8b8b8ecc6c39
zuojj <cuew1987@163.com>
2017-10-04 12:59:34 +0800

    update

--- webpack.demos/demo03/index.js
+++
@@ -1,9 +0,0 @@
-/**
- *
- * @authors Benjamin (zuojj.com@gmail.com)
- * @date    2016-11-12 21:32:30
- * @version $Id$
- */
-
-require('./index.scss');
-document.querySelector('.index-body').innerHTML = 'Body';
