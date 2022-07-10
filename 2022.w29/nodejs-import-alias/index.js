a9ecd780d3053cf5feca603599d9b237b462f830
junjunzuo <junjunzuo@tencent.com>
2022-07-10 09:53:12 +0800

    feat: nodejs import alias

---
+++ 2022.w29/nodejs-import-alias/index.js
@@ -0,0 +1,4 @@
+import { sum, plus } from '#util';
+
+console.log(`1 + 2 = ${sum(1,2)}`);
+console.log(`3 - 1 = ${plus(3,1)}`);
---
+++ 2022.w29/nodejs-import-alias/util/index.js
@@ -0,0 +1,7 @@
+export const sum = (a, b) => {
+  return a + b;
+}
+
+export const plus = (a, b) => {
+  return a - b;
+}
